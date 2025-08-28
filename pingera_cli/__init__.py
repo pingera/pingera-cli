0571e41b01d01aa48ada625169b3cc323cf6edf0
Sergey Pronin <spron-in@users.noreply.github.com>
2025-08-28 16:11:22 +0000

    minor wording fixes

--- pingera_cli/__init__.py
+++ pingera_cli/__init__.py
@@ -1,9 +1,9 @@
 """
-PingeraCLI - A beautiful CLI tool for Pingera Platform
+Pingera CLI - A beautiful CLI tool for Pingera Platform
 """
 
-__version__ = "0.1.0"
-__author__ = "PingeraCLI Team"
+__version__ = "0.1.1"
+__author__ = "Pingera Team"
 __description__ = "A beautiful Python CLI tool built with typer and rich, distributed via pip and based on Pingera SDK"
 
 from .main import app
