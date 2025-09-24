93633c739eacaa0a9eb6d71b75862adaa462ef4d
Sergey Pronin <spron-in@users.noreply.github.com>
2025-09-24 13:43:36 +0000

    latest ver

--- pingera_cli/__init__.py
+++ pingera_cli/__init__.py
@@ -2,7 +2,7 @@
 Pingera CLI - A beautiful CLI tool for Pingera Platform
 """
 
-__version__ = "0.1.8"
+__version__ = "0.1.9"
 __author__ = "Pingera Team"
 __description__ = "A beautiful Python CLI tool built with typer and rich, distributed via pip and based on Pingera SDK"
 
